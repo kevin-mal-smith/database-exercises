e660964f7b5b41e3ea47fa6e7fbd5a1501f09fde
kevin-mal-smith <kevin.mal.smith@gmail.com>
2022-05-18 17:50:45 -0400

    basic select function exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,11 +1,13 @@
 use albums_db;
 describe albums;
 select * from albums;
+select count(*) from albums;
 -- there are 31 rows and 6 columns in the albums table.
 select distinct artist from albums;
 -- there are 23 unique artists in the albums table.
 -- the primary key for the albums table is 'id.'
-select distinct release_date from albums;
+select min(release_date) from albums;
+select max(release_date) from albums;
 -- the oldest album was 1967 and the most recent was 2011.
 select name from albums where artist = "Pink Floyd";
 select release_date from albums where name = "Sgt. Pepper's Lonely Hearts Club Band";
