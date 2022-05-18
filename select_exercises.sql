39831fd455ba2979805f73009587b66696ac815d
kevin-mal-smith <kevin.mal.smith@gmail.com>
2022-05-18 17:54:00 -0400

    basic select function exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -12,7 +12,7 @@ select max(release_date) from albums;
 select name from albums where artist = "Pink Floyd";
 select release_date from albums where name = "Sgt. Pepper's Lonely Hearts Club Band";
 select genre from albums where name = "Nevermind";
-select * from albums where release_date >1989;
+select * from albums where release_date >1989 and release_date <2000;
 select * from albums where sales < 20;
 select * from albums where genre = "Rock";
 -- this query only shows albums which have a single genre of 'rock.' to include albums with multiple genres, or sub-genres of rock the where clause should be "where genre like "%Rock%"."
