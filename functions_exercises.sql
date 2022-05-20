4e45517b3625ac544e350325705424c9d34457f6
kevin-mal-smith <kevin.mal.smith@gmail.com>
2022-05-20 11:28:09 -0400

    CLEANED UP SQL basic functions exercises completed

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,23 +1,27 @@
 use employees;
 
 -- Write a query to to find all employees whose last name starts and ends with 'E'. Use concat() to combine their first and last name together as a single column named full_name.
-select concat(first_name," ", last_name) as full_name from employees
+select first_name, last_name, concat(first_name," ", last_name) as full_name from employees
 where last_name like "E%E";
+-- 899 records
 
 -- Convert the names produced in your last query to all uppercase.
-select upper(concat(first_name," ", last_name))
+select first_name, last_name, upper(concat(first_name," ", last_name))
  as full_name from employees
 where last_name like "E%E";
+-- 899 results
+
 
 -- Find all employees hired in the 90s and born on Christmas. Use datediff() function to find how many days they have been working at the company (Hint: You will also need to use NOW() or CURDATE())
-select datediff(curdate(),hire_date) 
+select *, datediff(curdate(),hire_date) as tenure
 from employees
 where (birth_date like "%12-25"
 and hire_date like "199%");
 
 -- Find the smallest and largest current salary from the salaries table.
 select min(salary), max(salary)
-from salaries;
+from salaries
+where to_date > curdate();
 
 -- Use your knowledge of built in SQL functions to generate a username for all of the employees. A username should be all lowercase, and consist of the first character of the employees first name, the first 4 characters of the employees last name, an underscore, the month the employee was born, and the last two digits of the year that they were born. 
 select lower(concat(substr(first_name, 1,1), 
